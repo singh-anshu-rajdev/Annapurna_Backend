b14f74ebe8da20bda315d751f82d10ffc0a09d13
anshu <anshu0007singh@gmail.com>
2024-10-05 23:08:00 +0530

    Adding cors Configuration

---
+++ src/main/java/com/annapurna/annapurna/config/WebConfiguration.java
@@ -0,0 +1,17 @@
+package com.annapurna.annapurna.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
+}
